9a5a9a7a1d2a7f5f41b40d4439e993bee8dd60e8
Greg Gibeling <gdgib@outlook.com>
2018-02-11 20:15:40 -0800

    G2-185 Cleanup maven dependency parsing

--- bd-build/src/main/java/com/g2forge/bulldozer/build/incomplete/IMaven.java
+++ bd-build/src/main/java/com/g2forge/bulldozer/build/incomplete/IMaven.java
@@ -1,10 +1,11 @@
 package com.g2forge.bulldozer.build.incomplete;
 
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import com.g2forge.gearbox.functional.control.Flag;
 import com.g2forge.gearbox.functional.control.Working;
 
 public interface IMaven {
-	public String mvn(@Working Path path, @Flag("-Dverbose") boolean verbose, String... goals);
+	public Stream<String> mvn(@Working Path path, @Flag("-Dverbose") boolean verbose, String... goals);
 }
--- bd-build/src/main/java/com/g2forge/bulldozer/build/incomplete/Test.java
+++ bd-build/src/main/java/com/g2forge/bulldozer/build/incomplete/Test.java
@@ -3,9 +3,15 @@ package com.g2forge.bulldozer.build.incomplete;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.g2forge.alexandria.java.core.helpers.HCollection;
 import com.g2forge.gearbox.functional.proxy.Proxifier;
 import com.g2forge.gearbox.functional.runner.ProcessBuilderRunner;
 
@@ -13,15 +19,18 @@ public class Test {
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 		final Path path = Paths.get(args[0]).resolve("enigma");
 		final IMaven maven = new Proxifier().generate(new ProcessBuilderRunner("cmd", "/C"), IMaven.class);
-		System.out.println(maven.mvn(path, true, "dependency:tree", "-Dincludes=com.g2forge.*:*"));
-		
+		final Map<String, List<Descriptor>> grouped = maven.mvn(path, true, "dependency:tree", "-Dincludes=com.g2forge.*:*")/*.map(new TapFunction<>(System.out::println))*/.filter(line -> line.startsWith("[INFO]") && line.contains("- com.g2forge.")).map(line -> Descriptor.fromString(line.substring(line.indexOf("- ") + 2))).collect(Collectors.groupingBy(Descriptor::getGroup));
+		final Map<String, String> versions = new LinkedHashMap<>();
+		for (List<Descriptor> group : grouped.values()) {
+			final Set<String> groupVersions = group.stream().map(Descriptor::getVersion).collect(Collectors.toSet());
+			if (groupVersions.size() > 1) throw new IllegalArgumentException(String.format("Depended on multiple versions of the project \"%1$s\": %2$s", group.get(0).getGroup(), groupVersions));
+			versions.put(group.get(0).getGroup(), HCollection.getOne(groupVersions));
+		}
+		System.out.println(versions);
+
 		// Do something to figure out what OS we're on the generate the shell prefix (bash vs cmd)
-		// Do something smarter and more type safe for running maven to get dependency trees (includes is a csv list, and the only argument, verbose should be a flag, the goal should be hard-coded)
-		// Read and parse the lines as they come in
-		// Make sure the maven command is successful
-		// Extract all other projects we found references to, and their versions (which better be only one version per project!)
-		// Only look at lines the start with "[INFO]" and contain the below prefix (exact match), THEN regex to extract version, starting from index of exact match for group (a little tricky with the option paren for omitted dependencies)
-		// - (com.g2forge.alexandria:ax-java:jar:0.0.7-SNAPSHOT:compile - omitted for duplicate)
-		// - com.g2forge.alexandria:ax-type:jar:0.0.7-SNAPSHOT:compile
+
+		// Do something smarter and more type safe for running maven to get dependency trees (includes is a csv list, and the only argument, verbose should be a
+		// flag, the goal should be hard-coded)
 	}
 }
