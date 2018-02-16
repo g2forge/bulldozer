90a59d845387d0eb08f8c830599eea8e6ca0068c
Greg Gibeling <gdgib@outlook.com>
2018-02-16 08:25:11 -0800

    G2-106 Added links to github repositories

--- bd-build/src/main/java/com/g2forge/bulldozer/build/Catalog.java
+++ bd-build/src/main/java/com/g2forge/bulldozer/build/Catalog.java
@@ -8,20 +8,27 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.event.Level;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.g2forge.alexandria.log.HLog;
+import com.g2forge.bulldozer.build.github.GitHubRepositoryID;
 import com.g2forge.bulldozer.build.model.BulldozerProject;
 import com.g2forge.bulldozer.build.model.Context;
+import com.g2forge.enigma.document.DocList;
+import com.g2forge.enigma.document.Link;
 import com.g2forge.enigma.document.Span;
 import com.g2forge.enigma.document.Span.SpanBuilder;
 import com.g2forge.enigma.document.Text;
 import com.g2forge.enigma.document.convert.md.MDRenderer;
+import com.g2forge.gearbox.git.GitConfig;
 
 import lombok.Data;
 
 @Data
 public class Catalog {
+
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException, GitAPIException {
 		final Catalog catalog = new Catalog(new Context(Paths.get(args[0])));
 		catalog.catalog();
@@ -30,16 +37,20 @@ public class Catalog {
 	protected final Context context;
 
 	public void catalog() throws IOException, GitAPIException {
+		HLog.getLogControl().setLogLevel(Level.INFO);
 		final Map<String, BulldozerProject> projects = getContext().loadProjects(BulldozerProject::new);
 
-		final com.g2forge.enigma.document.List.ListBuilder list = com.g2forge.enigma.document.List.builder().marker(com.g2forge.enigma.document.List.Marker.Ordered);
+		final DocList.DocListBuilder list = DocList.builder().marker(DocList.Marker.Ordered);
 		final List<String> ordered = new ArrayList<>(projects.keySet());
 		Collections.sort(ordered);
 		for (String name : ordered) {
 			final SpanBuilder item = Span.builder();
 
 			final BulldozerProject project = projects.get(name);
-			item.content(new Text(project.getPom().getName()));
+
+			final GitHubRepositoryID repositoryID = GitHubRepositoryID.fromURL(new GitConfig(project.getGit()).getOrigin().getURL());
+			item.content(new Link(repositoryID.toWebsiteURL(), new Text(project.getPom().getName())));
+
 			final String description = project.getPom().getDescription();
 			if (description != null) item.content(new Text(" - " + description));
 
