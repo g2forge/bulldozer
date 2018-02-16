2689e774e23ff0bc3f188a0b9183db85bc380e54
Greg Gibeling <gdgib@outlook.com>
2018-02-16 08:44:51 -0800

    G2-106 Separate sections per protection

--- bd-build/src/main/java/com/g2forge/bulldozer/build/Catalog.java
+++ bd-build/src/main/java/com/g2forge/bulldozer/build/Catalog.java
@@ -16,8 +16,11 @@ import com.g2forge.alexandria.log.HLog;
 import com.g2forge.bulldozer.build.github.GitHubRepositoryID;
 import com.g2forge.bulldozer.build.model.BulldozerProject;
 import com.g2forge.bulldozer.build.model.Context;
+import com.g2forge.bulldozer.build.model.maven.MavenProject;
+import com.g2forge.enigma.document.Block;
 import com.g2forge.enigma.document.DocList;
 import com.g2forge.enigma.document.Link;
+import com.g2forge.enigma.document.Section;
 import com.g2forge.enigma.document.Span;
 import com.g2forge.enigma.document.Span.SpanBuilder;
 import com.g2forge.enigma.document.Text;
@@ -38,24 +41,40 @@ public class Catalog {
 
 	public void catalog() throws IOException, GitAPIException {
 		HLog.getLogControl().setLogLevel(Level.INFO);
+		final Block.BlockBuilder projectsBuilder = Block.builder();
+
 		final Map<String, BulldozerProject> projects = getContext().loadProjects(BulldozerProject::new);
 
-		final DocList.DocListBuilder list = DocList.builder().marker(DocList.Marker.Ordered);
+		// Create a list of the project names and sort it alphabetically
 		final List<String> ordered = new ArrayList<>(projects.keySet());
 		Collections.sort(ordered);
-		for (String name : ordered) {
-			final SpanBuilder item = Span.builder();
 
-			final BulldozerProject project = projects.get(name);
+		// Loop over all the different protection profiles
+		for (MavenProject.Protection protection : MavenProject.Protection.values()) {
+			// Build a list of projects in this protection profile
+			final DocList.DocListBuilder listBuilder = DocList.builder().marker(DocList.Marker.Ordered);
+			for (String name : ordered) {
+				final BulldozerProject project = projects.get(name);
+				// This is where we skip projects not in this protection profile (could use streaming groupby in the future)
+				if (!protection.equals(project.getProject().getProtection())) continue;
 
-			final GitHubRepositoryID repositoryID = GitHubRepositoryID.fromURL(new GitConfig(project.getGit()).getOrigin().getURL());
-			item.content(new Link(repositoryID.toWebsiteURL(), new Text(project.getPom().getName())));
+				final SpanBuilder item = Span.builder();
 
-			final String description = project.getPom().getDescription();
-			if (description != null) item.content(new Text(" - " + description));
+				// Link to the github page for the repository
+				final GitHubRepositoryID repositoryID = GitHubRepositoryID.fromURL(new GitConfig(project.getGit()).getOrigin().getURL());
+				item.content(new Link(repositoryID.toWebsiteURL(), new Text(project.getPom().getName())));
 
-			list.item(item.build());
+				// Pull the description from the pom
+				final String description = project.getPom().getDescription();
+				if (description != null) item.content(new Text(" - " + description));
+
+				listBuilder.item(item.build());
+			}
+			// If we found any projects in this protection profile, then generate a section with the list of projects
+			final DocList list = listBuilder.build();
+			if (!list.getItems().isEmpty()) projectsBuilder.content(Section.builder().title(new Text(protection.name())).body(list).build());
 		}
-		System.out.println(new MDRenderer().render(list.build()));
+
+		System.out.println(new MDRenderer().render(Section.builder().title(new Text("Projects")).body(projectsBuilder.build()).build()));
 	}
 }
