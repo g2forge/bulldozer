edf3a0eb506b7c769945e5a22a3050a4d49a9886
Greg Gibeling <gdgib@outlook.com>
2026-02-22 22:09:51 -0800

    G2-1870 Skip tests during release install

--- bd-build/src/main/java/com/g2forge/bulldozer/build/Release.java
+++ bd-build/src/main/java/com/g2forge/bulldozer/build/Release.java
@@ -269,7 +269,7 @@ public class Release implements IConstructorCommand {
 					// Check out the recent tag using jgit
 					project.checkoutTag(project.getReleaseProperties().getTag());
 					// Maven install (stream stdio to the console) the newly created release version
-					getContext().getMaven().install(project.getDirectory(), IMaven.PROFILES_RELEASE);
+					getContext().getMaven().install(project.getDirectory(), true, IMaven.PROFILES_RELEASE);
 					phase = project.updatePhase(Phase.InstalledRelease);
 				}
 				log.info("Installed release {} {}", name, releaseProperties.getRelease());
@@ -327,7 +327,7 @@ public class Release implements IConstructorCommand {
 					// Check out the branch head
 					project.getGit().checkout().setCreateBranch(false).setName(getBranch()).call();
 					// Maven install (stream stdio to the console) the new development versions
-					getContext().getMaven().install(project.getDirectory(), IMaven.PROFILES_RELEASE);
+					getContext().getMaven().install(project.getDirectory(), false, IMaven.PROFILES_RELEASE);
 
 					log.info("Updating downstream {}", name);
 					for (BulldozerProject downstream : getContext().getProjects().values()) {
@@ -361,7 +361,7 @@ public class Release implements IConstructorCommand {
 				log.info("\t{}", name);
 				final BulldozerProject project = getContext().getNameToProject().get(name);
 				// Maven install (stream stdio to the console) the downstream, since it was updated
-				getContext().getMaven().install(project.getDirectory());
+				getContext().getMaven().install(project.getDirectory(), false, HCollection.emptyList());
 			}
 
 			// Find the local maven repository
